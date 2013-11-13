8b0b48f040496e87e88335358e8a21aa54f9795c
Alexander Yakushev <alex@bytopia.org>
2013-11-13 13:32:41 +0100

    Prepare 0.0.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compliment "0.0.2"
+(defproject compliment "0.0.3"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
