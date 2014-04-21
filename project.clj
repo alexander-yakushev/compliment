ce52a9c78a1b19734780820e05bb8de24f87f4de
Bozhidar Batsov <bozhidar@tradeo.com>
2014-04-21 09:45:29 +0300

    Add travis config

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.1.3"]]}
              :test-reflection [:dev
                                {:global-vars {*warn-on-reflection* true}}]})
