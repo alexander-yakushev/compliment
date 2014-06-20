dd1f6c279cae6b0d0a80f50f748793c8718dbd38
Alexander Yakushev <alex@bytopia.org>
2014-06-20 17:56:36 +0300

    Add source for special forms

---
+++ src/compliment/sources/special_forms.clj
@@ -0,0 +1,26 @@
+(ns compliment.sources.special-forms
+  "Completion for Clojure's special forms."
+  (:require [clojure.repl :as repl]
+            [compliment.sources :refer [defsource]]
+            [compliment.sources.ns-mappings :as vars]))
+
+(def ^:private special-forms
+  (map name '[def if do let quote var fn loop recur throw try catch
+              monitor-enter monitor-exit doto new set!]))
+
+(defn candidates
+  "Returns list of completions for special forms."
+  [prefix _ _]
+  (when (vars/var-symbol? prefix)
+    (for [form special-forms
+          :when (vars/dash-matches? prefix form)]
+      form)))
+
+(defn doc
+  "Documentation function for special forms."
+  [symbol-str _]
+  (vars/generate-docstring (#'repl/special-doc (symbol symbol-str))))
+
+(defsource ::special-forms
+  :candidates #'candidates
+  :doc doc)
---
+++ test/compliment/sources/t_special_forms.clj
@@ -0,0 +1,17 @@
+(ns compliment.sources.t-special-forms
+  (:require [midje.sweet :refer :all]
+            [compliment.sources.special-forms :as src]))
+
+(facts "about special forms"
+  (fact "special forms are matched everywhere and use dash as fuzzy separator"
+    (src/candidates "va" *ns* nil)
+    => ["var"]
+
+    (src/candidates "c" *ns* nil)
+    => ["catch"]
+
+    (src/candidates "mo-e" *ns* nil)
+    => ["monitor-enter" "monitor-exit"])
+
+  (fact "there are docs for special forms too"
+    (src/doc "try" *ns*) => string?))
