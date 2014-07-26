f2fca4d506917dc6384ae77cd4a68c1714634b95
Alexander Yakushev <alex@bytopia.org>
2014-07-26 22:50:09 +0300

    Add other Clojure versions to test matrix

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+                   :plugins [[lein-midje "3.1.3"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :aliases {"test-all" ["do" ["check"] ["midje"]]})
