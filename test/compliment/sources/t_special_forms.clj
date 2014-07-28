1a068e11b938d97d4ba713fc58be0149fda9c0b2
Alexander Yakushev <alex@bytopia.org>
2014-07-28 16:17:56 +0300

    Fix tests for special forms

--- test/compliment/sources/t_special_forms.clj
+++ test/compliment/sources/t_special_forms.clj
@@ -1,18 +1,22 @@
 (ns compliment.sources.t-special-forms
   (:require [midje.sweet :refer :all]
+            [compliment.context :as ctx]
             [compliment.sources.special-forms :as src]))
 
 (facts "about special forms"
-  (fact "special forms are matched everywhere and use dash as fuzzy separator"
-    (src/candidates "va" *ns* nil)
+  (fact "special forms are matched when they are first item in the list"
+    (src/candidates "va" *ns* (ctx/parse-context '(__prefix__ foo)))
     => ["var"]
 
-    (src/candidates "c" *ns* nil)
+    (src/candidates "c" *ns* (ctx/parse-context '(__prefix__ Exception ex nil)))
     => ["catch"]
 
-    (src/candidates "mo-e" *ns* nil)
-    => ["monitor-enter" "monitor-exit"])
+    (src/candidates "mo-e" *ns* (ctx/parse-context '(__prefix__)))
+    => ["monitor-enter" "monitor-exit"]
+
+    (src/candidates "" *ns* (ctx/parse-context '(str __prefix__ 42)))
+    => nil)
 
   (fact "there are docs for special forms too"
     (src/doc "try" *ns*) => string?
-    (src/doc "not-a-form" => nil)))
+    (src/doc "not-a-form" *ns*) => nil))
