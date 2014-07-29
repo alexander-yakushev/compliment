7cf7453c43911a819de27660c03837b63e673c1f
Alexander Yakushev <alex@bytopia.org>
2014-07-29 11:12:38 +0300

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compliment "0.1.3"
+(defproject compliment "0.1.4-SNAPSHOT"
   :description "The Clojure completion library you deserve"
   :url "https://github.com/alexander-yakushev/compliment"
   :license {:name "Eclipse Public License"
