fd1814c15f2a52628c65c3bb8c325ca6d72e2cd4
Alexander Yakushev <alex@bytopia.org>
2014-08-26 16:02:58 +0200

    Forgo Clojure 1.4

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/alexander-yakushev/compliment"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-SNAPSHOT"]]}}
   :aliases {"test-all" ["do" ["check"] ["midje"]]})
