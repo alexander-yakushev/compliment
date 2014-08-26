8f1140c15bf7b057564ddfc547beac60759a73e3
Alexander Yakushev <alex@bytopia.org>
2014-08-26 17:06:38 +0200

    Fix Clojure 1.7 version

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-SNAPSHOT"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha1"]]}}
   :aliases {"test-all" ["do" ["check"] ["midje"]]})
