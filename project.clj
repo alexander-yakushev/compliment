bc21799d3d74f498100aede330648de7e0c95be3
Alexander Yakushev <alex@bytopia.org>
2018-10-16 13:53:48 +0300

    Test with Clojure 1.10

--- project.clj
+++ project.clj
@@ -19,5 +19,5 @@
                                          ["shell" "curl" "-F"
                                           "json_file=@target/coverage/coveralls.json"
                                           "https://coveralls.io/api/v1/jobs"]]}}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-beta3"]]}})
