c475a488d5a41cbcd2d92be71f4fbedd5518d75e
Bozhidar Batsov <bozhidar@batsov.dev>
2022-06-17 12:12:46 +0300

    Bump the development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject compliment "0.3.12"
+(defproject compliment "0.3.13-SNAPSHOT"
   :description "The Clojure completion library you deserve"
   :url "https://github.com/alexander-yakushev/compliment"
   :license {:name "Eclipse Public License"
