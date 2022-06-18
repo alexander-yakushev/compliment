8a15fdfcb56011eeb6a5ae26d715bc11207239e0
Bozhidar Batsov <bozhidar@batsov.dev>
2022-06-18 22:13:08 +0300

    Update the deployment config

--- project.clj
+++ project.clj
@@ -3,6 +3,12 @@
   :url "https://github.com/alexander-yakushev/compliment"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]]
+
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [criterium "0.4.4"]
                                   [cloverage "1.0.13"]
