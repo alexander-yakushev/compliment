8aa91abfdad045a8444ab11183eedc2a050a8444
Alexander Yakushev <alex@bytopia.org>
2023-11-07 13:14:39 +0200

    [g.s.classes] Refactor and optimize

--- src/compliment/sources/classes.clj
+++ src/compliment/sources/classes.clj
@@ -3,15 +3,8 @@
   (:require [compliment.sources :refer [defsource]]
             [compliment.utils :refer [fuzzy-matches?] :as utils]
             [compliment.sources.class-members :refer [classname-doc]]
-            [compliment.sources.namespaces :refer [nscl-symbol? nscl-matches?]]))
-
-;;; Obtaining the list of classes
-
-(defn imported-classes
-  "Returns names of all classes imported into a given namespace."
-  [ns]
-  (for [[_ ^Class val] (ns-map ns) :when (class? val)]
-    (.getName val)))
+            [compliment.sources.namespaces :refer [nscl-symbol? nscl-matches?]])
+  (:import java.util.HashSet))
 
 (defn- all-classes-short-names
   "Returns a map where short classnames are matched with vectors with
@@ -20,7 +13,7 @@
   (let [all-classes (utils/classes-on-classpath)]
     (utils/cache-last-result :all-classes-short-names all-classes
       (group-by (fn [^String s] (.substring s (inc (.lastIndexOf s "."))))
-                (apply concat (vals all-classes))))))
+                all-classes))))
 
 (defn- analyze-import-context
   "Checks if the completion is called from ns import declaration. If so, and the
@@ -36,70 +29,94 @@
                (not= prefix-form import-list))
       (str (first prefix-form)))))
 
-(defn- get-all-full-names
-  "Returns a list of package-qualified classnames given a short classname."
-  [prefix]
-  (reduce-kv (fn [l, ^String short-name, full-names]
-               (if (.startsWith short-name prefix)
-                 (into l (map (fn [c] {:candidate c, :type :class})) full-names)
-                 l))
-             []
-             (all-classes-short-names)))
-
 (defn- get-classes-by-package-name
   "Returns simple classnames that match the `prefix` and belong to `pkg-name`."
   [prefix pkg-name]
   (reduce-kv (fn [l, ^String short-name, full-names]
                (if (and (.startsWith short-name prefix)
-                        (some #(.startsWith ^String % pkg-name) full-names))
+                        (some (fn [^String fn] (.startsWith fn pkg-name))
+                              full-names))
                  (conj l {:candidate short-name, :type :class})
                  l))
-             []
-             (all-classes-short-names)))
+             [] (all-classes-short-names)))
+
+;; The function below is quite ugly for performance and efficiency reasons. The
+;; total number of classes can go up to 7 digits, and we must still return the
+;; result reasonably quickly in such scenarios.
 
 (defn candidates
   "Returns a list of classname completions."
   [^String prefix, ns context]
   (when (nscl-symbol? prefix)
-    (let [has-dot (> (.indexOf prefix ".") -1)]
-      (if-let [import-ctx (analyze-import-context context)]
-        (get-classes-by-package-name prefix import-ctx)
+    (if-let [import-ctx (analyze-import-context context)]
+      (get-classes-by-package-name prefix import-ctx)
 
-        (into []
-              (comp cat (distinct))
-              [(for [class-str (imported-classes ns)
-                     :when (nscl-matches? prefix class-str)]
-                 {:candidate class-str, :type :class})
+      (let [has-dot (.contains prefix ".")
+            seen (HashSet.)
+            include? (fn ([class-str remember?]
+                          (when-not (.contains seen class-str)
+                            (when remember? (.add seen class-str))
+                            true)))
+            str->cand (fn [s] {:candidate s, :type :class})
+            all-classes (utils/classes-on-classpath)
+            it (.iterator ^Iterable all-classes)
+            roots (utils/root-packages-on-classpath)]
 
-               (for [[_ ^Class val] (ns-map ns) :when (class? val)
-                     :let [sname (.getSimpleName val)]
-                     :when (nscl-matches? prefix sname)]
-                 {:candidate sname, :type :class,
-                  :package (when-let [pkg (.getPackage ^Class val)]
-                             ;; Some classes don't have a package
-                             (.getName ^Package pkg))})
+        (as-> (transient []) result
+          ;; For classes imported into the namespace, suggest them both as
+          ;; short names and fully-qualified names.
+          (reduce-kv
+           (fn [result _ v]
+             (if (class? v)
+               (let [fqname (.getName ^Class v)
+                     sname (.getSimpleName ^Class v)]
+                 (cond-> result
+                   (and (nscl-matches? prefix fqname) (include? fqname true))
+                   (conj! (str->cand fqname))
 
-               ;; For capitalized prefix, complete class FQN from a short name.
-               (when (Character/isUpperCase (.charAt prefix 0))
-                 (get-all-full-names prefix))
+                   (and (nscl-matches? prefix sname) (include? sname true))
+                   (conj! {:candidate sname, :type :class,
+                           :package (when-let [pkg (.getPackage ^Class v)]
+                                      ;; Some classes don't have a package
+                                      (.getName ^Package pkg))})))
+               result))
+           result (ns-map ns))
 
-               ;; Fuzziness is too slow for all classes, so only startsWith. Also, if no
-               ;; period in prefix, only complete root package names to maintain good
-               ;; performance and not produce too many candidates.
-               (let [all-classes (utils/classes-on-classpath)]
-                 (if (or has-dot (contains? all-classes prefix))
-                   (for [[root-pkg classes] all-classes
-                         :when (.startsWith prefix root-pkg)
-                         ^String cl-str classes
-                         :when (.startsWith cl-str prefix)]
-                     {:candidate cl-str, :type :class})
-                   (for [[^String root-pkg _] all-classes
-                         :when (.startsWith root-pkg prefix)]
-                     {:candidate (str root-pkg "."), :type :class})))])))))
+          ;; For capitalized prefix, complete class FQN from a short name.
+          (if (Character/isUpperCase (.charAt prefix 0))
+            (reduce-kv (fn [result, ^String short-name, full-names]
+                         (if (.startsWith short-name prefix)
+                           (reduce (fn [result cl]
+                                     (cond-> result
+                                       (include? cl true) (conj! (str->cand cl))))
+                                   result full-names)
+                           result))
+                       result (all-classes-short-names))
+            result)
+
+          ;; Fuzziness is too slow for all classes, so only startsWith. Also, if
+          ;; no period in prefix, only complete root package names to maintain
+          ;; good performance and not produce too many candidates.
+          (if (or has-dot (contains? roots prefix))
+            (loop [result result]
+              (if (.hasNext it)
+                (let [^String cl (.next it)]
+                  (recur (cond-> result
+                           (and (.startsWith cl prefix) (include? cl false))
+                           (conj! (str->cand cl)))))
+                result))
+
+            (reduce conj! result
+                    (for [^String root-pkg roots
+                          :when (and (.startsWith root-pkg prefix)
+                                     (include? root-pkg false))]
+                      (str->cand (str root-pkg ".")))))
+
+          (persistent! result))))))
 
 (defn doc [class-str curr-ns]
   (when (nscl-symbol? class-str)
-    (some-> (utils/resolve-class curr-ns class-str) classname-doc)))
+    (some-> (utils/resolve-class curr-ns (symbol class-str)) classname-doc)))
 
 (defsource ::classes
   :candidates #'candidates
