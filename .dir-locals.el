d14d25c2bf4ee044a3942017fa4bd3b62d3d9141
Oleksandr Yakushev <alex@bytopia.org>
2024-08-14 00:59:46 +0300

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil
+  (cider-clojure-cli-aliases . "dev:test"))
+ (clojure-mode
+  (clojure-indent-style . :always-align)
+  (indent-tabs-mode . nil)
+  (fill-column . 80)
+  (clojure-docstring-fill-column . 80)))
